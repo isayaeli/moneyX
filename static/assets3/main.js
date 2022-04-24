dcc67e20835ce6ed62f7600d9613b9a0da0065d1
isayaeli <isayaelib@gmail.com>
2022-04-24 09:30:48 +0300

    big UI updates

---
+++ static/assets3/main.js
@@ -0,0 +1,18 @@
+$(document).on('click', function(){
+    $('#sidelink').css('font-weight','bold')
+})
+
+function aos_init() {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false
+    });
+  }
+  window.addEventListener('load', () => {
+    aos_init();
+  });
+
+
+
