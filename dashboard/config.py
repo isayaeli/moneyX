3bc7ee074cf62188c8316f70f268947917d38ed8
isayaeli <isayaelib@gmail.com>
2022-05-01 13:17:27 +0300

    configuring ok authentication

---
+++ dashboard/config.py
@@ -0,0 +1,47 @@
+import hmac
+import base64 as Base64
+import requests
+import json
+from datetime import datetime
+import datetime
+
+CONTENT_TYPE = 'Content-Type'
+OK_ACCESS_KEY = 'OK-ACCESS-KEY'
+OK_ACCESS_SIGN = 'OK-ACCESS-SIGN'
+OK_ACCESS_TIMESTAMP = 'OK-ACCESS-TIMESTAMP'
+OK_ACCESS_PASSPHRASE = 'OK-ACCESS-PASSPHRASE'
+APPLICATION_JSON = 'application/json'
+
+# signature
+def signature(timestamp, method, request_path, body, secret_key):
+    if str(body) == '{}' or str(body) == 'None':
+        body = ''
+    message = str(timestamp) + str.upper(method) + request_path + str(body)
+    mac = hmac.new(bytes(secret_key, encoding='utf8'), bytes(message, encoding='utf-8'), digestmod='sha256')
+    d = mac.digest()
+    return Base64.b64encode(d)
+
+
+# set request header
+def get_header(api_key, sign, timestamp, passphrase):
+    header = dict()
+    header[CONTENT_TYPE] = APPLICATION_JSON
+    header[OK_ACCESS_KEY] = api_key
+    header[OK_ACCESS_SIGN] = sign
+    header[OK_ACCESS_TIMESTAMP] = str(timestamp)
+    header[OK_ACCESS_PASSPHRASE] = passphrase
+    return header
+
+
+def parse_params_to_str(params):
+    url = '?'
+    for key, value in params.items():
+        url = url + str(key) + '=' + str(value) + '&'
+
+    return url[0:-1]
+
+
+api_key = 'c1f727c6-55b6-4cbb-ba6c-2a54649cb476'
+secret_key = '52F8998D968C14D145B3BE8DA7A1E5BF'
+timestamp = f"{datetime.datetime.utcnow().isoformat()[:-3]}Z"
+pass_phrase = 'Isaya@mimi100%'
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+import requests
+from .config import *
 # Create your views here.
 def index(request):
     return render(request, 'dash/dash.html')
@@ -10,4 +11,22 @@ def deposits(request):
 
 
 def withdraws(request):
-    return render(request,'dash/withdraws.html')
+    base_url = 'https://www.okcoin.com'
+    request_path = '/api/account/v3/wallet'
+
+    params = {'currency':'BTC'}
+
+    # request path
+    request_path = request_path + parse_params_to_str(params)
+    url = base_url + request_path
+
+    # request header and body
+    header = get_header(api_key, signature(timestamp, 'GET', request_path,{},secret_key), timestamp, pass_phrase)
+
+    # do request
+    response = requests.get(url, headers=header)
+    print(response.json())
+    context = {
+        'balance': response.json()
+    }
+    return render(request,'dash/withdraws.html', context)
