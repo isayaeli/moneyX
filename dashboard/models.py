3ecb1029b2afa44f1ba2acfa23194cc3c8513cdc
isayaeli <isayaelib@gmail.com>
2022-05-24 13:23:06 +0300

    Added changes to deposit history

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,6 +1,5 @@
 from datetime import datetime
-from itertools import chain
-from tabnanny import verbose
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -15,7 +14,13 @@ class AssetBalance(models.Model):
         verbose_name_plural = 'Asset balance'
 
 class DepositHistory(models.Model):
+    STATUS =  (
+        ('succesful','successful'),
+        ('not-successful','not-successful')
+    )
+    user =  models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     history = models.JSONField(null=True)
+    status =  models.CharField(choices=STATUS, max_length=50,default='not-successful')
     saved_on = models.DateTimeField(default=datetime.now)
 
     def __str__(self):
