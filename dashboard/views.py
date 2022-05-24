3eac762db33860dd5dcd50e0195d0a558b18f00d
isayaeli <isayaelib@gmail.com>
2022-05-24 15:32:53 +0300

    clean up codes

--- dashboard/views.py
+++ dashboard/views.py
@@ -53,8 +53,8 @@ def deposits(request):
 
     saved_history = saved_history[0]['history']
     for data in history:
-        for save_data in saved_history:
-            saved = save_data['timestamp']
+        for saved_data in saved_history:
+            saved = saved_data['timestamp']
         from_api = data['timestamp']
         
     if saved != from_api or saved == None:
